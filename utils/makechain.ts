04917643264cfbfd1f51fd0da56f9b3c49e88b55
bbh <b.benhassine@hotmail.com>
2023-05-08 20:47:35 +0200

    0000

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,24 +2,14 @@ import { OpenAI } from 'langchain/llms/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+ 
 
-Chat History:
+const CONDENSE_PROMPT_FR = `À partir de la conversation suivante et de la question de suivi, reformulez la question de suivi pour en faire une question autonome.
+
+Historique de conversation :
 {chat_history}
-Follow Up Input: {question}
-Standalone question:`;
-
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-
-{context}
-
-Question: {question}
-Helpful answer in markdown:`;
-
-
-const CONDENSE_PROMPT_FR = ``;
+Question de suivi : {question}
+Question autonome : `;
 
 const QA_PROMPT_FR = ` 
 
@@ -27,11 +17,10 @@ En tant qu'assistant virtuel sur le site MOBILI-JEUNE, votre rôle est crucial p
 Veuillez fournir des réponses en français et vous adresser à l'utilisateur de manière courtoise.
 
 Votre rôle est limité à aider les utilisateurs dans leur demande d'aide MOBILI-JEUNE, et non à fournir des informations en dehors de ce contexte.
-Utilisez les informations fournies par MOBILI-JEUNE pour répondre aux questions des utilisateurs et soyez courtois, professionnel et empathique avec eux.
+Utilisez les informations fournies dans le contexte pour répondre aux questions des utilisateurs et soyez courtois, professionnel et empathique avec eux.
 N'offrez pas d'informations ou de conseils en dehors de ce qui est précisé dans le contexte.
-Si la demande de l'utilisateur n'est pas claire ou pertinente, demandez-lui de reformuler sa demande sous forme de question pour mieux comprendre ses besoins et y répondre de manière adéquate.
+Si la demande de l'utilisateur n'est pas claire ou pertinente, demandez-lui de reformuler sa demande sous forme de question pour mieux comprendre ses besoins.
  
-
 
 {context}
 
@@ -52,7 +41,7 @@ export const makeChain = (vectorstore: PineconeStore) => {
     {
       qaTemplate: QA_PROMPT_FR,
       questionGeneratorTemplate: CONDENSE_PROMPT_FR,
-      returnSourceDocuments: false, //The number of source documents returned is 4 by default
+      returnSourceDocuments: true, //The number of source documents returned is 4 by default
     },
   );
   return chain;
